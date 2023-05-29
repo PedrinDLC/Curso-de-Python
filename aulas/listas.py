272397cb1d8b29b4738c3f5734e25687c5fbeb23
Pedro Luiz <pedrohenriqueluiz3@gmail.com>
2023-05-29 20:37:19 -0300

    Update aula de listas

--- aulas/listas.py
+++ aulas/listas.py
@@ -15,18 +15,6 @@ Já em Python:
 - Qualquer tipo de dado: Não posuem um tipo de dado fixo; Ou seja, podemos colocar qualquer tipo de dado;
 
 As listas em python são representadas por colchetes: []
-"""
-type([])
-
-lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
-
-lista2 = ['G', 'e', 'e', 'k', ' ', 'U', 'n', 'i', 'v', 'e', 'r', 's', 'i', 't', 'y']
-
-lista3 = []
-
-lista4 = list(range(11))
-
-lista5 = list('Geek University')
 
 # Podemos facilmente checar se determinado valor está contido na lista
 num = 7
@@ -45,13 +33,107 @@ print(lista5.count('e'))
 
 # Adicionar elementos em listas
 
-"""
-Para Adicionar elementos em listas, utilizamos a função append
+# Para Adicionar elementos em listas, utilizamos a função append
 
-OBS: Com append, nós só conseguimos adicionar 1 elemento por vez
-"""
 print(lista1)
 lista1.append(42)
 print(lista1)
 
-lista1.append(12, 34, 56)  # Erro
+# OBS: Com append, nós só conseguimos adicionar 1 elemento por vez
+# lista1.append(12, 34, 56)  # Erro
+
+lista1.append([8, 3, 1])  # Coloca a lista como elemento único (sublista)
+print(lista1)
+
+if [8, 3, 1] in lista1:
+    print('Encontrei a lista')
+else:
+    print('Não encontrei a lista')
+
+lista1.extend([123, 44, 67])  # Coloca cada elemento da lista como valor adicional à lista
+print(lista1)
+
+# Podemos inserir um novo elemento na lista informando a posição do índice
+# OBS: Isso não substitui o valor inicial, o mesmo será deslocado para a direita da lista.
+lista1.insert(2, 'Novo Valor')
+print(lista1)
+
+# Podemos facilmente juntar duas listas
+
+lista1 = lista1 + lista2
+# lista1.extend(lista2)
+print(lista1)
+
+# Podemos facilmente inverter uma lista
+
+# Forma 1
+lista1.reverse()
+lista2.reverse()
+print(lista1)
+print(lista2)
+
+# Forma 2
+print(lista1[::-1])
+print(lista2[::-1])
+
+# Copiar uma lista
+
+lista6 = lista2.copy()
+print(lista6)
+
+# Podemos contar quantos elementos existem dentro da lista
+print(len(lista5))
+
+# Podemos remover facilmente o ultimo elemento de uma lista
+# OBS: O pop não somente remove o ultimo elemento mas tambem o retorna
+print(lista5)
+lista5.pop()
+print(lista5)
+
+# podemos remover um elemento pelo indice
+# OBS: Os elementos à direita deste indice serão deslocados para a esquerda.
+# OBS: Se não houver elemento no indice informado teremos o erro IndexError.
+lista5.pop(2)
+print(lista5)
+
+# Podemos remover todos os elementos (zerar a lista)
+print(lista5)
+lista5.clear()
+print(lista5)
+
+# Podemos facilmente repetir elementos em uma lista
+nova = [1, 2, 3]
+print(nova)
+nova = nova * 3
+print(nova)
+
+# Podemos facilmente converter string para uma lista
+
+# Exemplo 1
+
+curso = 'Programação em Python: Essencial'
+print(curso)
+curso = curso.split()
+print(curso)
+
+# OBS: Por padrão o split separa os elementos da lista pelo espaço entre elas.
+
+# Exemplo 2
+curso = 'Programação,em,Python:,Essencial'
+print(curso)
+curso = curso.split(',')
+print(curso)
+
+"""
+type([])
+
+lista1 = [1, 99, 4, 27, 15, 22, 3, 1, 44, 42, 27]
+
+lista2 = ['G', 'e', 'e', 'k', ' ', 'U', 'n', 'i', 'v', 'e', 'r', 's', 'i', 't', 'y']
+
+lista3 = []
+
+lista4 = list(range(11))
+
+lista5 = list('Geek University')
+
